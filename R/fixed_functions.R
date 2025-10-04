50aa4e52261b3c8f71b50fd18105c4655c0f87de
Noah Bolohan <noah.bolohan@gmail.com>
2025-10-04 14:38:13 -0400

    Added some missed functions

--- R/fixed_functions.R
+++ R/fixed_functions.R
@@ -430,3 +430,269 @@ pwhl_team_roster <- function(team, season, regular = TRUE) {
 
   return(roster_data)
 }
+
+#' @title  **PWHL Schedule**
+#' @description PWHL Schedule lookup
+#'
+#' @param season Season (YYYY) to pull the schedule from, the concluding year in XXXX-YY format
+#' @return A data frame with schedule data
+#' @import jsonlite
+#' @import dplyr
+#' @import httr
+#' @importFrom glue glue
+#' @export
+#' @examples
+#' \donttest{
+#'   try(pwhl_schedule(season = 2023))
+#' }
+
+pwhl_schedule <- function(season, game_type = "regular") {
+
+  seasons <- pwhl_season_id() %>%
+    dplyr::filter(season_yr == season, game_type_label == game_type)
+
+  season_id <- seasons$season_id
+
+  base_url = glue::glue(
+    "https://lscluster.hockeytech.com/feed/index.php?feed=statviewfeed&view=schedule&team=-1&season={season_id}&month=-1&location=homeaway&key=694cfeed58c932ee&client_code=pwhl&site_id=2&league_id=1&division_id=-1&lang=en&callback=angular.callbacks._1"
+  )
+  full_url = base_url
+
+  res <- httr::RETRY(
+    "GET",
+    full_url
+  )
+
+  res <- res %>%
+    httr::content(as = "text", encoding = "utf-8")
+  callback_pattern <- "angular.callbacks._\\d+\\("
+  res <- gsub(callback_pattern, "", res)
+  # res <- gsub("\\}\\]\\)$", "}}]", res)
+  # res <- gsub("angular.callbacks._1\\(", "", res)
+  res <- gsub("}}]}]}])", "}}]}]}]", res)
+
+  r <- res %>%
+    jsonlite::parse_json()
+
+  gm <- r[[1]]$sections[[1]]$data
+
+  schedule_data <- data.frame()
+
+  tryCatch(
+    expr = {
+      for (i in 1:length(gm)) {
+
+        if (is.null(gm[[i]]$prop$venue_name$venueUrl)) {
+          venue <-'TBD'
+        } else {
+          venue <- gm[[i]]$prop$venue_name$venueUrl
+        }
+
+        game_info <- data.frame(
+          "game_id" = c(gm[[i]]$row$game_id),
+          "season" = c(season),
+          "game_date" = c(gm[[i]]$row$date_with_day),
+          "game_status" = c(gm[[i]]$row$game_status),
+          "home_team" = c(gm[[i]]$row$home_team_city),
+          "home_team_id" = c(gm[[i]]$prop$home_team_city$teamLink),
+          "away_team" = c(gm[[i]]$row$visiting_team_city),
+          "away_team_id" = c(gm[[i]]$prop$visiting_team_city$teamLink),
+          "home_score" = c(gm[[i]]$row$home_goal_count),
+          "away_score" = c(gm[[i]]$row$visiting_goal_count),
+          "venue" = c(gm[[i]]$row$venue_name),
+          "venue_url" = c(venue)
+        )
+
+        schedule_data <- rbind(
+          schedule_data,
+          game_info
+        )
+
+      }
+
+      schedule_data <- schedule_data %>%
+        dplyr::mutate(
+          winner = dplyr::case_when(
+            .data$home_score == '' | .data$away_score == "-" ~ '-',
+            .data$home_score > .data$away_score ~ .data$home_team,
+            .data$away_score > .data$home_score ~ .data$away_team,
+            .data$home_score == .data$away_score & .data$home_score != "-" ~ "Tie",
+            TRUE ~ NA_character_
+          ),
+          season = season
+        ) %>%
+        dplyr::select(
+          c(
+              "game_id",
+              "game_date",
+              "game_status",
+              "home_team",
+              "home_team_id",
+              "away_team",
+              "away_team_id",
+              "home_score",
+              "away_score",
+              "winner",
+              "venue",
+              "venue_url"
+            )
+        )
+    },
+    error = function(e) {
+      message(glue::glue("{Sys.time()}: Invalid season or no schedule data available! Try a season from 2023 onwards!"))
+
+    },
+    warning = function(w) {
+    },
+    finally = {
+    }
+  )
+
+  return(schedule_data)
+
+}
+
+#' @title  **PWHL Season IDs**
+#' @description PWHL Season IDs lookup
+#'
+#' @return A data frame with season ID data
+#' @import jsonlite
+#' @import dplyr
+#' @import httr
+#' @importFrom glue glue
+#' @export
+#' @examples
+#' \donttest{
+#'   try(pwhl_season_id())
+#' }
+
+pwhl_season_id <- function(season, game_type = "regular") {
+
+  season_id <- data.frame(
+    "season_yr" = c(2024, 2024, 2024, 2025, 2025, 2025),
+    "game_type_label" = c("preseason", "regular", "playoffs",
+                    "preseason", "regular", "playoffs"),
+    "season_id" = c(2, 1, 3, 4, 5, 6)
+  )
+
+  return(season_id)
+
+}
+
+#' @title  **PWHL Player Game Box Scores**
+#' @description PWHL Player Box Scores
+#'
+#' @param game_id Game ID that you want play-by-play for
+#' @return A data frame with play-by-play data from the PWHL
+#' @import jsonlite
+#' @import dplyr
+#' @import httr
+#' @importFrom glue glue
+#' @export
+#' @examples \donttest{
+#'   try(pwhl_player_box(game_id = 27))
+#' }
+
+pwhl_player_box <- function(game_id) {
+
+  tryCatch(
+    expr = {
+      URL <- glue::glue("https://lscluster.hockeytech.com/feed/index.php?feed=statviewfeed&view=gameSummary&game_id={game_id}&key=694cfeed58c932ee&site_id=2&client_code=pwhl&lang=en&league_id=&callback=angular.callbacks._6")
+
+      res <- httr::RETRY("GET", URL)
+      res <- res %>%
+        httr::content(as = "text", encoding = "utf-8")
+      callback_pattern <- "angular.callbacks._\\d+\\("
+      res <- gsub(callback_pattern, "", res)
+      res <- gsub("}}})", "}}}", res)
+
+      r <- res %>% jsonlite::parse_json()
+
+      suppressWarnings(
+        expr = {
+
+        home_goalie <- data.frame(row.names = NULL, (do.call(rbind, lapply(r$homeTeam$goalies, function(x) unlist(x))))) %>% janitor::clean_names()
+        away_goalie <- data.frame(row.names = NULL, (do.call(rbind, lapply(r$visitingTeam$goalies, function(x) unlist(x))))) %>% janitor::clean_names()
+
+        home_skaters <- data.frame(row.names = NULL, (do.call(rbind, lapply(r$homeTeam$skaters, function(x) unlist(x))))) %>% janitor::clean_names()
+        away_skaters <- data.frame(row.names = NULL, (do.call(rbind, lapply(r$visitingTeam$skaters, function(x) unlist(x))))) %>% janitor::clean_names()
+
+      })
+
+      home_goalie$team_id <- r$homeTeam$info$id
+      away_goalie$team_id <- r$visitingTeam$info$id
+      home_skaters$team_id <- r$homeTeam$info$id
+      away_skaters$team_id <- r$visitingTeam$info$id
+
+      # home_skatersi
+
+      goalies <- dplyr::bind_rows(home_goalie, away_goalie)
+      skaters <- dplyr::bind_rows(home_skaters, away_skaters)
+      skaters$game_id <- r$details$id
+      goalies$game_id <- r$details$id
+
+      skaters <- skaters %>%
+        dplyr::select(-c("info_birth_date", "info_jersey_number", "info_player_image_url", "status")) %>%
+        dplyr::mutate(league = "pwhl")
+
+      goalies <- goalies %>%
+        dplyr::select(-c("info_birth_date", "info_jersey_number", "info_player_image_url", "status")) %>%
+        dplyr::mutate(starting = ifelse(starting != "1", 0, 1)) %>%
+        dplyr::mutate(league = "pwhl")
+
+      skater_cols <- c("player_id", "first_name", "last_name", "position", "goals", "assists", "points", "penalty_minutes", "plus_minus", "faceoff_attempts",
+                       "faceoff_wins", "shots", "hits", "blocked_shots", "toi", "starting", "team_id", "game_id", "league")
+      goalie_cols <- c("player_id", "first_name", "last_name", "position", "goals", "assists", "points", "penalty_minutes", "plus_mins", "faceoff_attempts",
+                       "faceoff_wins", "toi", "shots_against", "goals_against", "saves", "starting", "team_id", "game_id", "league")
+      colnames(skaters) <- skater_cols
+      colnames(goalies) <- goalie_cols
+
+      suppressWarnings(
+        expr = {
+
+        skaters <- skaters %>%
+          dplyr::mutate_at(c( "goals", "assists", "points", "penalty_minutes", "plus_minus", "faceoff_attempts",
+                              "faceoff_wins", "shots", "hits", "blocked_shots"), as.numeric) %>%
+          dplyr::mutate(
+            faceoff_losses = faceoff_attempts - faceoff_wins,
+            faceoff_pct = faceoff_wins / faceoff_attempts
+          ) %>%
+          tidyr::separate(toi, into = c("minute", "second"), remove = FALSE) %>%
+          dplyr::mutate(time_on_ice = round((as.numeric(minute) * 60 + as.numeric(second)) / 60, 1)) %>%
+          dplyr::select(c("player_id", "first_name", "last_name", "position", "team_id", "game_id", "league", "toi", "time_on_ice",
+                          "goals", "assists", "points", "shots", "hits", "blocked_shots",
+                          "penalty_minutes", "plus_minus", "faceoff_attempts", "faceoff_wins", "faceoff_losses", "faceoff_pct", "starting"))
+
+        goalies <- goalies %>%
+          dplyr::mutate_at(c( "goals", "assists", "points", "penalty_minutes", "faceoff_attempts",
+                              "faceoff_wins", "saves", "shots_against",
+                              "goals_against"), as.numeric) %>%
+          dplyr::mutate(
+            faceoff_losses = faceoff_attempts - faceoff_wins,
+            faceoff_pct = faceoff_wins / faceoff_attempts
+          ) %>%
+          tidyr::separate(toi, into = c("minute", "second"), remove = FALSE) %>%
+          dplyr::mutate(time_on_ice = round((as.numeric(minute) * 60 + as.numeric(second)) / 60, 1)) %>%
+          dplyr::select(c("player_id", "first_name", "last_name", "position", "team_id", "game_id", "league", "toi", "time_on_ice",
+                          "saves", "goals_against", "shots_against",
+                          "goals", "assists", "points",
+                          "penalty_minutes", "faceoff_attempts", "faceoff_wins", "faceoff_losses", "faceoff_pct", "starting"))
+
+        })
+
+      player_box <- c(list(skaters), list(goalies))
+      names(player_box) <- c("skaters", "goalies")
+    },
+    error = function(e) {
+      message(glue::glue("{Sys.time()}: Invalid game_id or no player box data available! Try using `phf_schedule` to find a game ID to look up!"))
+    },
+    warning = function(w) {
+
+    },
+    finally = {
+
+    }
+  )
+
+  return(player_box)
+}
