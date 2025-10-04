0e4cef0aae51bf7ce7c689e13152936d801498e9
alexmcsw <alexmcsw@gmail.com>
2025-10-04 13:49:01 -0400

    ugh

--- R/team_stats.R
+++ R/team_stats.R
@@ -1,25 +1,26 @@
 # create a nice function to lapply to our team vector
 
-
 # get data
 team_stats <- function(
-    season
+  season
 ) {
- 
   # get vector of team abbreviations
-  teams <- fastRhockey::pwhl_teams()$team_label
+  teams <- pwhl_teams()$team_label
 
   all_teams <- data.frame()
 
-  for (team in teams){
+  for (team in teams) {
+    # here we use our modified functions
+    df_stats <- pwhl_stats_fix(
+      position = "skater",
+      team = team,
+      season = season
+    )
 
-  # here we use our modified functions
-  df_stats <- pwhl_stats_fix(position = "skater", team = team, season = season)
-
-  df_team <- pwhl_team_roster(
-    team = team,
-    season = season
-  ) %>%
+    df_team <- pwhl_team_roster(
+      team = team,
+      season = season
+    ) %>%
       mutate(
         sign = DescTools::Zodiac(as.Date(dob))
       ) %>%
@@ -27,11 +28,10 @@ team_stats <- function(
         df_stats,
         by = c("player_id")
       )
-    
+
     all_teams <- rbind(all_teams, df_team) |>
       filter(current_team == 1)
   }
 
   return(all_teams)
-
 }
