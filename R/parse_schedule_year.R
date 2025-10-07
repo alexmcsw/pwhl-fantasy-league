d149811e1abc7778c0b2000cccd1b5677f41721a
Noah Bolohan <noah.bolohan@gmail.com>
2025-10-07 14:34:29 -0400

    Overview page

---
+++ R/parse_schedule_year.R
@@ -0,0 +1,22 @@
+parse_schedule_year <- function(
+    game_date,
+    season_start
+) {
+
+    game_date_with_year <- ymd(
+        paste0(
+            "2024 ",
+            game_date
+        )
+    )
+    
+    if (
+        game_date_with_year < season_start
+    ) {
+        year(game_date_with_year) <- year(game_date_with_year) + 1
+    }
+
+    return (
+        game_date_with_year
+    )
+}
