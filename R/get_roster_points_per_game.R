069c92a29c6d251a2df75dba6d7b42d019bb2fa4
Noah Bolohan <noah.bolohan@gmail.com>
2025-10-10 20:35:41 -0400

    Dashboard renders before preseason

--- R/get_roster_points_per_game.R
+++ R/get_roster_points_per_game.R
@@ -3,16 +3,42 @@ get_roster_points_per_game <- function(
     player_boxes_per_game
 ) {
 
-    return(
-        player_boxes_per_game |> map(
-            get_roster_points_from_game,
-            team_rosters = team_rosters
-        ) |> bind_rows() |> mutate(
-            game_id = names(
-                player_boxes_per_game
+    if (
+        length(
+            player_boxes_per_game
+        ) == 0
+    ) {
+
+        return(
+            roster_points_per_game <- data.frame(
+                matrix(
+                    ncol = length(
+                        team_rosters
+                    ),
+                    nrow = 0,
+                    dimnames = list(
+                        NULL,
+                        names(
+                            team_rosters
+                        )
+                    )
+                )
             )
-        ) |> column_to_rownames(
-            "game_id"
         )
-    )
+
+    } else {
+
+        return(
+            player_boxes_per_game |> map(
+                get_roster_points_from_game,
+                team_rosters = team_rosters
+            ) |> bind_rows() |> mutate(
+                game_id = names(
+                    player_boxes_per_game
+                )
+            ) |> column_to_rownames(
+                "game_id"
+            )
+        )
+    }
 }
