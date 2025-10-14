202bb5604c5f3e56312ac151d3d7e2898adbbd68
Noah Bolohan <noah.bolohan@gmail.com>
2025-10-14 07:24:22 -0400

    get_schedule()

--- R/get_schedule.R
+++ R/get_schedule.R
@@ -1,8 +1,33 @@
+library(stringr)
+library(purrr)
+library(lubridate)
+library(dplyr)
+
+#' @title  **Get PWHL Schedule and Nearby Game Dates**
+#' @description Get PWHL schedule and next/last game dates
+#'
+#' @param season_id Current season ID
+#' @param team_codes Named list of shortened PWHL team codes
+#' @param team_logo_urls Named list of URLs of PWHL team logos
+#' @param season_dates_and_type data.frame of current season dates and type
+#' @param last_game_day_date Last game date flag
+#' @param next_game_day_date Next game date flag
+#' @param current_date Current date
+#' @return data.frames of schedule and next/last game days
+#' @import stringr
+#' @import purrr
+#' @import lubridate
+#' @import dplyr
+#' @export
+
 get_schedule <- function(
   season_id,
   team_codes,
   team_logo_urls,
-  season_dates_and_type
+  season_dates_and_type,
+  last_game_day_date,
+  next_game_day_date,
+  current_date
 ) {
   schedule <- pwhl_schedule(
     season_id = season_id
@@ -57,5 +82,62 @@ get_schedule <- function(
       ]
     )
 
-  return(schedule)
+  if (
+    as.character(
+      last_game_day_date
+    ) ==
+      "tbd"
+  ) {
+    last_game_day_date <- Filter(
+      function(x) ymd(x) < ymd(current_date),
+      schedule$game_date
+    ) |>
+      last()
+  }
+
+  if (
+    is.null(
+      last_game_day_date
+    )
+  ) {
+    last_game_day <- FALSE
+  } else {
+    last_game_day <- schedule |>
+      filter(
+        game_date ==
+          ymd(
+            last_game_day_date
+          )
+      )
+  }
+
+  if (
+    as.character(
+      next_game_day_date
+    ) ==
+      "tbd"
+  ) {
+    next_game_day_date <- Filter(
+      function(x) ymd(x) >= ymd(current_date),
+      schedule$game_date
+    )[[1]]
+  }
+
+  next_game_day <- schedule |>
+    filter(
+      game_date ==
+        ymd(
+          next_game_day_date
+        )
+    )
+
+  return(
+    list(
+      schedule = schedule,
+      last_game_day_date = last_game_day_date,
+      next_game_day_date = next_game_day_date,
+      last_game_day = last_game_day,
+      next_game_day = next_game_day
+    )
+  )
 }
