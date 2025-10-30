af9f6fdbbaad251d97989d4948180be132d86c34
Noah Bolohan <noah.bolohan@gmail.com>
2025-10-30 14:49:08 -0400

    Headshots always show

---
+++ R/generate_pwhl_roster_gt_table.R
@@ -0,0 +1,62 @@
+library(dplyr)
+library(gt)
+
+#" @title  **Generate PWHL Roster GT Table**
+#" @description Generate the GT table for the PWHL Rosters page
+#"
+#" @param team_label PWHL team label
+#" @param all_teams All PWHL team rosters
+#" @return GT table of PWHL teams roster
+#" @import dplyr
+#" @import gt
+#" @export
+
+generate_pwhl_roster_gt_table <- function(
+    all_teams,
+    team_label
+) {
+    return(
+        all_teams |>
+            filter(
+                team == team_label
+            ) |>
+            mutate(
+                position = recode(
+                    position,
+                    "F" = "Forwards",
+                    "D" = "Defenders",
+                    "G" = "Goalies"
+                )
+            ) |>
+            select(
+                c(
+                    "jersey_number",
+                    "player_headshot",
+                    "player_name",
+                    "position",
+                    "primary_hand",
+                    "dob",
+                    "home_town"
+                )
+            ) |>
+            rename(
+                c(
+                    "#" = "jersey_number",
+                    Headshot = "player_headshot",
+                    Name = "player_name",
+                    Pos = "position",
+                    Shoots = "primary_hand",
+                    DOB = "dob",
+                    "Home town" = "home_town"
+                )
+            ) |>
+            gt(
+                groupname_col = "Pos"
+            ) |>
+            fmt_image(
+                columns = "Headshot",
+                width = "50px",
+                height = "50px"
+            )
+    )
+}
